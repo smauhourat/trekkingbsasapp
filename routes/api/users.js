f1628ef3266ff26f4737aced6a09e5cd072cfbe3
SMauhourat <santiagomauhourat@hotmail.com>
2022-03-16 20:41:32 -0300

    save user in db fix

--- routes/api/users.js
+++ routes/api/users.js
@@ -29,7 +29,7 @@ router.post(
             let user = await User.findOne({ email: email });
 
             if (user) {
-                res.status(400).json({ errors: [{msg: 'User already exists'}] })
+                return res.status(400).json({ errors: [{msg: 'User already exists'}] })
             }
 
             // Encrypt password
