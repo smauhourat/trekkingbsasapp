07c23cd1caabbe9ae7e1147769c6e1abbcb21ffc
Santiago Mauhourat <smauhourat@aadim.org.ar>
2022-03-16 15:51:40 -0300

    add validation to register user api 2

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator/check');
 
+const User = require('../../models/User');
+
 // @route   POST api/users
 // @desc    Register route
 // @access  Public
@@ -11,10 +13,24 @@ router.post(
         check('name', 'Name is required').not().isEmpty(),
         check('email', 'Please include a valid email').isEmail(),
         check('password', 'Please enter a password with 6 or more characters').isLength({ min:6 })
-    ],(req,res) => {
+    ],
+    async (req,res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
           return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { name, email, password } = req.body;
+
+        try {
+            // See if user exists
+
+            // Encrypt password
+
+            // Return jsonwebtoken
+
+        } catch(err) {
+
         }
 
         res.send('User router')
