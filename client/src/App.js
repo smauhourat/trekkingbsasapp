242da6656d3fcd49016ef4ce6c5797a4fcec1fdd
Santiago Mauhourat <smauhourat@aadim.org.ar>
2022-03-28 16:26:01 -0300

    add router dom in app

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import './App.css';
 
-const App = () =>
-    <Fragment>
-      <Navbar />
-      <Landing />
-    </Fragment>
+const App = () => (
+  <Router>
+    <Navbar />
+    <Routes>
+      <Fragment>
+        {/* <Navbar /> */}
+        <Route exact path="/" element={<Landing/>} />
+      </Fragment>
+    </Routes>
+  </Router>
+);
 
 export default App;
