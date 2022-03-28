8d51a14a064970f9d8b98a8fe450988339b093e3
Santiago Mauhourat <smauhourat@aadim.org.ar>
2022-03-28 18:24:22 -0300

    fill login component

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,9 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 const Login = () => {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    });
+
+    const { email, password } = formData;
+
+    const onChange = e => 
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+
+    const onSubmit = async e => {
+        e.preventDefault();
+        console.log('SUCCESS');
+    }
+
   return (
     <section className="container">
-        <h1>Login</h1>
+      <div className="alert alert-danger">
+        Credenciales inválidas
+      </div>
+      <h1 className="large text-primary">Ingreso</h1>
+      <p className="lead"><i className="fas fa-user"></i> Ingrese a su cuenta</p>
+      <form className="form" onSubmit={e => onSubmit(e)}>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            value={email}
+            onChange={e => onChange(e)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Contraseña"
+            name="password"
+            value={password}
+            onChange={e => onChange(e)}
+            minLength="6"
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Ingresar" />
+      </form>
     </section>
   );
 };
