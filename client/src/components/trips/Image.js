d2bc2eb86baf4142205b80f815311e3fd1c0413b
Santiago Mauhourat <smauhourat@aadim.org.ar>
2022-05-05 17:42:58 -0300

    images fit

--- client/src/components/trips/Image.js
+++ client/src/components/trips/Image.js
@@ -5,7 +5,8 @@ const Image = ({ id, url }) => {
   return (
     <li key={id}>
       <figure>
-        <img src={url.replace('upload', 'upload/ar_1.0,c_limit,h_150')} />
+        {/* <img src={url.replace('upload', 'upload/ar_1.0,c_limit,h_150')} /> */}
+        <img src={url} />
         <figcaption><h3><a href={url} target="_blank">tqebqtkibaapsc310hzi</a></h3></figcaption>
       </figure>
       <p>
