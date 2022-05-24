0698f3e9359d81cfe994b41a01b01a57add9638d
Santiago Mauhourat <smauhourat@aadim.org.ar>
2022-05-24 18:21:34 -0300

    show category in trip details

--- client/src/components/trips/TripItem.js
+++ client/src/components/trips/TripItem.js
@@ -18,25 +18,29 @@ const TripItem = ({
 }) => {
   return (
     <div className="profile bg-white">
-    <img
-      className="round-img-slow"
-      src={trip.images[0]?.url}
-      alt=""
-    />
-    <div>
-      <h2>{trip.title}</h2>
-      <p>{trip.subtitle}</p>
-      <p>{trip.location}</p>
-      <Link to={'/trip-details/'+trip._id} state={{data: trip}} className='btn btn-primary'>
-        <i className='text-primary' /> Ver Detalle
-      </Link>      
+      <img
+        className="round-img-slow"
+        src={trip.images[0]?.url}
+        alt=""
+      />
+      <div>
+        <h2>{trip.title}</h2>
+        <p>{trip.subtitle}</p>
+        <p>{trip.location}</p>
+        <Link to={'/trip-details/'+trip._id} state={{data: trip}} className='btn btn-primary'>
+          <i className='text-primary' /> Ver Detalle
+        </Link>      
+      </div>
+      <div>
+        {trip.category
+          &&
+          (<span><h3>Categoria:</h3><div>{trip.category}</div></span>)
+        }
+        <h3>Fecha:</h3>{formatDate(trip.date)}
+        <h3>Duración:</h3>{trip.duration}
+        <h3>Disponibilidad:</h3>{trip.quota} lugares
+      </div>
     </div>
-    <div>
-      <h3>Fecha:</h3>{formatDate(trip.date)}
-      <h3>Duración:</h3>{trip.duration}
-      <h3>Disponibilidad:</h3>{trip.quota} lugares
-    </div>
-  </div>
   )
 }
 
