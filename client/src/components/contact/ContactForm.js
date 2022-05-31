6c67e46cf8a242252381dbfc3771b59a714d62cb
SMauhourat <santiagomauhourat@hotmail.com>
2022-05-30 21:47:29 -0300

    send contact form

--- client/src/components/contact/ContactForm.js
+++ client/src/components/contact/ContactForm.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import api from '../../utils/api'
 
 import PropTypes from 'prop-types'
 
 const ContactForm = props => {
     const navigate = useNavigate();
+
+    const [formData, setFormData] = useState({
+        title: '',
+        email: '',
+        subject: '',
+        message: ''
+      });    
+
+    const { title, email, subject, message } = formData;
+
+    const onChange = (e) =>
+      setFormData({ ...formData, [e.target.name]: e.target.value });     
+
+    const sendContactForm = (formData) => {
+        try {
+            api.post('/contact', formData);
+        } catch(err) {
+            console.error(err);
+        }
+    }
 
     return (
         <div>
@@ -15,15 +36,21 @@ const ContactForm = props => {
                 <p className="lead">
                     <i className="fas fa-hand-point-right"></i> En que podemos ayudarte?
                 </p>
-                <form className="form">
+                <form
+                    className="form"
+                    onSubmit={(e) => {
+                        e.preventDefault();
+                        sendContactForm(formData);
+                    }}
+                >
                     <div className="form-group">
-                    <input type="text" placeholder="Nombre" name="title" required />
+                        <input type="text" placeholder="Nombre" name="title" value={title} onChange={onChange} required />
                     </div>
                     <div className="form-group">
-                    <input type="text" placeholder="Email" name="email" required />
+                    <input type="text" placeholder="Email" name="email" value={email} onChange={onChange} required />
                     </div>
                     <div className="form-group">
-                    <input type="text" placeholder="Asunto" name="subject" />
+                    <input type="text" placeholder="Asunto" name="subject" value={subject} onChange={onChange}/>
                     </div>
                     <div className="form-group">
                     <textarea
@@ -31,6 +58,8 @@ const ContactForm = props => {
                         cols="30"
                         rows="5"
                         placeholder="Mensaje"
+                        value={message}
+                        onChange={onChange}
                     ></textarea>
                     </div>
                     <input type="submit" className="btn btn-primary my-1" />
@@ -40,11 +69,11 @@ const ContactForm = props => {
             
             <div className="footer-basic bg-dark">
                 <footer>
-                    <div className="social"><a href="#"><i className="fab fa-instagram"></i></a><a href="#"><i className="fab fa-facebook-f"></i></a><a href="#"><i className="fab fa-twitter"></i></a><a href="#"><i className="fab fa-youtube"></i></a></div>
+                    <div className="social"><a><i className="fab fa-instagram"></i></a><a><i className="fab fa-facebook-f"></i></a><a><i className="fab fa-twitter"></i></a><a><i className="fab fa-youtube"></i></a></div>
                     <ul className="list-inline">
                         <li className="list-inline-item"><Link to="/">Home</Link></li>
-                        <li className="list-inline-item"><a href="#">Calendario</a></li>
-                        <li className="list-inline-item"><a href="#">La Empresa</a></li>
+                        <li className="list-inline-item"><a>Calendario</a></li>
+                        <li className="list-inline-item"><a>La Empresa</a></li>
                         <li className="list-inline-item"><Link to="/contact">Contacto</Link></li>
                     </ul>
                     <p className="copyright">©TrekkingBsAs 2022 - Todos los Derechos Reservados</p>
---
+++ routes/api/contact.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const { getMaxListeners } = require('../../models/User');
+
+const config = require('config');
+
+const transporter = nodemailer.createTransport({
+    host: config.get('contact_host'), //replace with your email provider
+    port: config.get('contact_port'), //replace with your
+    auth: {
+      user: config.get('contact_user'), //process.env.EMAIL,
+      pass: config.get("contact_pwd") //process.env.PASSWORD
+    }
+  });
+
+// verify connection configuration
+transporter.verify(function(error, success) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Server is ready to take our messages");
+    }
+  });  
+
+// @route    GET api/contact
+// @desc     Send Contact Info
+// @access   Public
+router.post('/', 
+    [
+        check('title', 'Titulo es requerido').not().isEmpty(),
+        check('email', 'Email es requerido').not().isEmpty(),
+        check('subject', 'Asunto es requerido').not().isEmpty(),
+        check('message', 'Mensaje es requerido').not().isEmpty(),
+    ],
+    async (req,res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+          return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { title, email, subject, message } = req.body;
+
+        var mail = {
+            from: email,
+            to: config.get('contact_to'),
+            subject: subject,
+            text: 'De: ' + email + '\nMensaje: ' + message
+          }        
+        
+        transporter.sendMail(mail, (err, data) => {
+            if (err) {
+              res.json({
+                status: 'fail'
+              })
+            } else {
+              res.json({
+               status: 'success'
+              })
+            }
+          });
+
+        res.json(req.body);
+
+    }
+)
+
+module.exports = router;
