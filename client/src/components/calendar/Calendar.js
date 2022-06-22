1da66ed972630200200aecefd92050636f827994
Santiago Mauhourat <smauhourat@aadim.org.ar>
2022-06-22 17:31:19 -0300

    filter calendar by month

--- client/src/components/calendar/Calendar.js
+++ client/src/components/calendar/Calendar.js
@@ -1,27 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Trips from '../trips/Trips';
 
 const Calendar = () => {
+
+  const [selectedMonth, setSelectedMonth] = useState();
+
+  const handleMonthClick = (e) => {
+    setSelectedMonth(e);
+  }
+
   return (
-    <section class="container">
-        <h1 class="large text-primary">Calendario</h1>
-        <div class="nav-months">
+    <section className="container">
+        <h1 className="large text-primary">Calendario</h1>
+        <div className="nav-months">
             <ul>
-                <li><a class="disabled"> enero </a></li>
-                <li><a class="disabled"> febrero </a></li>
-                <li><a class="disabled"> marzo </a></li>
-                <li><a class="disabled"> abril </a></li>
-                <li><a class="disabled"> mayo </a></li>
-                <li><a class="active"> junio </a></li>
-                <li><a> julio </a></li>
-                <li><a> agosto </a></li>
-                <li><a> septiembre </a></li>
-                <li><a> octubre </a></li>
-                <li><a> noviembre </a></li>
-                <li><a> diciembre </a></li>
+                <li><a className="disabled"> enero </a></li>
+                <li><a onClick={() => handleMonthClick(2)}> febrero </a></li>
+                <li><a onClick={() => handleMonthClick(3)}> marzo </a></li>
+                <li><a onClick={() => handleMonthClick(4)}> abril </a></li>
+                <li><a onClick={() => handleMonthClick(5)}> mayo </a></li>
+                <li><a className="active" onClick={() => handleMonthClick(6)}> junio </a></li>
+                <li><a onClick={() => handleMonthClick(7)}> julio </a></li>
+                <li><a onClick={() => handleMonthClick(8)}> agosto </a></li>
+                <li><a onClick={() => handleMonthClick(9)}> septiembre </a></li>
+                <li><a onClick={() => handleMonthClick(10)}> octubre </a></li>
+                <li><a onClick={() => handleMonthClick(11)}> noviembre </a></li>
+                <li><a onClick={() => handleMonthClick(12)}> diciembre </a></li>
             </ul>
         </div>
-        <div class="my-1">&nbsp;</div>
-        <h1 class="lead">Actividades</h1>
+        {/* <div className="my-1">&nbsp;</div>
+        <h1 className="lead">Actividades</h1> */}
+        <Trips monthSearch={selectedMonth}/>
     </section>
   )
 }
--- client/src/components/trips/Trips.js
+++ client/src/components/trips/Trips.js
@@ -6,17 +6,49 @@ import TripItem from './TripItem';
 import { getTrips } from '../../actions/trip';
 import Spinner from '../layout/Spinner';
 
-const Trips = ({ getTrips, trip: { trips, loading } }) => {
+const Trips = ({ getTrips, trip: { trips, loading }, monthSearch }) => {
 
-    const queryParam = new URLSearchParams(useLocation().search).get("q");
-    const queryParam2 = new URLSearchParams(useLocation().search);
-    const query = queryParam2 !== undefined || queryParam2 !== null ? queryParam2 : '';
-    //debugger;
+    // const queryParam2 = new URLSearchParams(useLocation().search);
+    // let query = queryParam2 !== undefined || queryParam2 !== null ? queryParam2 : '';
     
+    // const isCalendar = useLocation().pathname.includes('calendar');
+    // const selectedMonth = monthSearch !== undefined ? monthSearch : '07';
+
+    // query = isCalendar ? 'df=2022-'+selectedMonth+'-01' : query;
+
+
+
+    // let fecha = new Date('12/01/2022');
+    // console.log(fecha);
+    // console.log(fecha.getMonth());
+
+    console.log(monthSearch);
+
+    function getQueryGral(arg) {
+      const params = new URLSearchParams(arg);
+      return params !== undefined || params !== null ? params : '';
+    }
+
+    function getQueryCalendar() {
+      const currentYear = (new Date()).getFullYear();
+      const currentMonth = (new Date()).getMonth() + 1;
+      const selectedMonth = monthSearch !== undefined ? monthSearch : currentMonth;
+
+      var lastDay = (new Date(currentYear, selectedMonth, 0)).getDate();
+
+      const ret = 'df=' + currentYear + '-' + selectedMonth + '-01&dt=' + currentYear + '-' + selectedMonth + '-' + lastDay;
+      console.log(ret);
+      return ret;
+    }
+
+    const isCalendar = useLocation().pathname.includes('calendar');
+    const search = useLocation().search;
+
+    const query = isCalendar ? getQueryCalendar() : getQueryGral(search);
 
     useEffect(() => {
         getTrips(query);
-    }, [getTrips])
+    }, [getTrips, monthSearch])
 
     return (
         <section className="container">
@@ -24,12 +56,21 @@ const Trips = ({ getTrips, trip: { trips, loading } }) => {
         <Spinner />
       ) : (            
         <Fragment>
-            <h1 className="large text-primary">Eventos</h1>
-            <p className="lead">
-            <i className="fa-solid fa-arrow-down-wide-short"></i> Resultado de la busqueda...
-            </p>
+            {!isCalendar ? (
+            <>
+              <h1 className="large text-primary">Eventos</h1>
+              <p className="lead">
+                <i className="fa-solid fa-arrow-down-wide-short"></i> Resultado de la busqueda...
+              </p>
+            </>
+            ) : (
+              <>
+                <div className="my-1">&nbsp;</div>
+                <h1 className="lead">Actividades</h1>
+              </>
+            )}
             <div className="profiles">
-            {trips.data?.length > 0 ? (
+            {trips?.data?.length > 0 ? (
                 trips.data.map((trip) => (
                 <TripItem key={trip._id} trip={trip} />
                 ))
@@ -48,8 +89,9 @@ Trips.propTypes = {
     trip: PropTypes.object.isRequired
   };
   
-  const mapStateToProps = (state) => ({
-    trip: state.trip
+  const mapStateToProps = (state, ownProps) => ({
+    trip: state.trip,
+    monthSearch: ownProps.monthSearch
   });
   
   export default connect(mapStateToProps, { getTrips })(Trips);
