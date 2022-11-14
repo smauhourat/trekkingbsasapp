8a99e4a17b0835a9b8af0aefeed8a94f771090b9
Santiago Mauhourat <smauhourat@aadim.org.ar>
2022-11-14 16:18:31 -0300

    add published to trips

--- client/src/components/trips/EditTrip.js
+++ client/src/components/trips/EditTrip.js
@@ -5,13 +5,13 @@ import { connect } from 'react-redux';
 import { updateTrip } from '../../actions/trip';
 import formatDateISO from '../../utils/formatDateISO';
 
-const EditTrip = ({ 
-    trip: {trips}, 
-    updateTrip
+const EditTrip = ({
+  trip: { trips },
+  updateTrip
 }) => {
-    const navigate = useNavigate();
-    
-    const [editedTrip, setEditedTrip] = useState({
+  const navigate = useNavigate();
+
+  const [editedTrip, setEditedTrip] = useState({
     title: '',
     subtitle: '',
     category: '',
@@ -27,190 +27,192 @@ const EditTrip = ({
     reservations: 0,
     published: true,
     suggested_equipment: ''
-  })    
+  })
 
-   
-      const id = useParams().id;
 
-      useEffect(() => {
-        const trip = trips.data.find(trip  => trip._id === id);
-        setEditedTrip(trip);
-      }, [id, trips.data]);
-      
-      const { title, subtitle, category, description, itinerary, date, duration, price, location, grading, quota, reservations, published, suggested_equipment } = editedTrip;
+  const id = useParams().id;
 
-      const onChange = (e) => {
-        if (e.target.name === 'date') {
-          console.log(e.target.value);
-        }        
-        setEditedTrip({ ...editedTrip, [e.target.name]: e.target.value });      
-      };
+  useEffect(() => {
+    const trip = trips.data.find(trip => trip._id === id);
+    setEditedTrip(trip);
+  }, [id, trips.data]);
 
-      return (
-        <section className="container">
-        <h1 className="large text-primary">Eventos</h1>
-        <p className="lead"><i className="fas fa-calendar"></i> Crear Evento</p>
-        <form
-                className="form"
-                onSubmit={(e) => {
-                e.preventDefault();
-                updateTrip(id, editedTrip, navigate);
-                }}
-            >
-          <div className="form-group">
-            <label>Titulo</label>
-            <input 
-                type="text" 
-                placeholder="Titulo" 
-                name="title" 
-                value={title}
-                onChange={onChange}
-                required />
-          </div>
-          <div className="form-group">
-            <label>Subtitulo</label>
-            <input 
-              type="text" 
-              placeholder="Subtitulo" 
-              name="subtitle" 
-              value={subtitle}
-              onChange={onChange}              
-            />
-          </div>
-          <div>
-            <label>Categoria</label>
-            <select name="category" value={category} onChange={onChange}>
-              <option>* Selecione una Categoria</option>
-              <option value="Trekking">Trekking</option>
-              <option value="Caminatas">Caminatas</option>
-              <option value="Mountain Bike">Mountain Bike</option>
-              <option value="Kayaks">Kayaks</option>
-            </select>            
-          </div>          
-          <div className="form-group">
+  const { title, subtitle, category, description, itinerary, date, duration, price, location, grading, quota, reservations, published, suggested_equipment } = editedTrip;
+
+  const onChange = (e) => {
+    if (e.target.name === 'date') {
+      console.log(e.target.value);
+    }
+    const newValue = e.target.type === 'checkbox' ? e.target.checked : e.target.value;
+
+    setEditedTrip({ ...editedTrip, [e.target.name]: newValue });
+  };
+
+  return (
+    <section className="container">
+      <h1 className="large text-primary">Eventos</h1>
+      <p className="lead"><i className="fas fa-calendar"></i> Crear Evento</p>
+      <form
+        className="form"
+        onSubmit={(e) => {
+          e.preventDefault();
+          updateTrip(id, editedTrip, navigate);
+        }}
+      >
+        <div className="form-group">
+          <label>Titulo</label>
+          <input
+            type="text"
+            placeholder="Titulo"
+            name="title"
+            value={title}
+            onChange={onChange}
+            required />
+        </div>
+        <div className="form-group">
+          <label>Subtitulo</label>
+          <input
+            type="text"
+            placeholder="Subtitulo"
+            name="subtitle"
+            value={subtitle}
+            onChange={onChange}
+          />
+        </div>
+        <div>
+          <label>Categoria</label>
+          <select name="category" value={category} onChange={onChange}>
+            <option>* Selecione una Categoria</option>
+            <option value="Trekking">Trekking</option>
+            <option value="Caminatas">Caminatas</option>
+            <option value="Mountain Bike">Mountain Bike</option>
+            <option value="Kayaks">Kayaks</option>
+          </select>
+        </div>
+        <div className="form-group">
           <label>Descripcion</label>
-            <textarea 
-              placeholder="Descripcion"
-              rows="5"
-              name="description"
-              value={description}
-              onChange={onChange}              
-            />
-          </div>
-          <div className="form-group">
+          <textarea
+            placeholder="Descripcion"
+            rows="5"
+            name="description"
+            value={description}
+            onChange={onChange}
+          />
+        </div>
+        <div className="form-group">
           <label>Itinerario</label>
-            <textarea 
-              placeholder="Itinerario"
-              rows="5"
-              name="itinerary"
-              value={itinerary}
-              onChange={onChange}              
-            />
-          </div>          
-          <div className="form-group">
-            <label>Equipo Sugerido</label>
-            <textarea
-              placeholder="Equipo Sugerido"
-              rows="5"
-              name="suggested_equipment"
-              value={suggested_equipment}
-              onChange={onChange}              
-            />
-          </div>          
-          <div className="form-group">
-            <label>Fecha</label>
-            <input
-              type="date"
-              placeholder="Fecha"
-              name="date"
-              value={formatDateISO(date)}
-              onChange={onChange}              
-            />
-          </div>
-          <div className="form-group">
-            <label>Duración</label>
-            <input
-              type="text"
-              placeholder="Duracion"
-              name="duration"
-              value={duration}
-              onChange={onChange}              
-            />
-          </div>
-          <div className="form-group">
-            <label>Precio</label>
-            <input
-              type="text"
-              placeholder="Precio"
-              name="price"
-              value={price}
-              onChange={onChange}              
-            />
-          </div>
-          <div className="form-group">
-            <label>Lugar</label>
-            <input
-              type="text"
-              placeholder="Lugar"
-              name="location"
-              value={location}
-              onChange={onChange}              
-            />
-          </div>
-          <div className="form-group">
-            <label>Grado Dificultad</label><small> (Valor numerico del 1 al 5)</small>
-            <input
-              type="text"
-              placeholder="Grado dificultad"
-              name="grading"
-              value={grading}
-              onChange={onChange}              
-            />
-          </div>
-          <div className="form-group">
-            <label>Cupo</label>
-            <input
-              type="text"
-              placeholder="Cupo"
-              name="quota"
-              value={quota}
-              onChange={onChange}              
-            />
-          </div>
-          <div className="form-group">
-            <label>Reservas</label>
-            <input
-              type="text"
-              placeholder="Reservas"
-              name="reservations"
-              value={reservations}
-              onChange={onChange}              
-            />                                    
-          </div>
-          <div className="form-group">
-            <label>Publicado</label>
-            <input
-              type="checkbox"
-              name="published"
-              checked={published}
-              onChange={onChange}              
-            />
-          </div>
-          <input type="submit" className="btn btn-primary" value="Aceptar" />
-          <input type="button" className="btn btn-secondary" value="Cancelar" onClick={() => navigate('/dashboard')} />
-        </form>
-      </section>
-    );
+          <textarea
+            placeholder="Itinerario"
+            rows="5"
+            name="itinerary"
+            value={itinerary}
+            onChange={onChange}
+          />
+        </div>
+        <div className="form-group">
+          <label>Equipo Sugerido</label>
+          <textarea
+            placeholder="Equipo Sugerido"
+            rows="5"
+            name="suggested_equipment"
+            value={suggested_equipment}
+            onChange={onChange}
+          />
+        </div>
+        <div className="form-group">
+          <label>Fecha</label>
+          <input
+            type="date"
+            placeholder="Fecha"
+            name="date"
+            value={formatDateISO(date)}
+            onChange={onChange}
+          />
+        </div>
+        <div className="form-group">
+          <label>Duración</label>
+          <input
+            type="text"
+            placeholder="Duracion"
+            name="duration"
+            value={duration}
+            onChange={onChange}
+          />
+        </div>
+        <div className="form-group">
+          <label>Precio</label>
+          <input
+            type="text"
+            placeholder="Precio"
+            name="price"
+            value={price}
+            onChange={onChange}
+          />
+        </div>
+        <div className="form-group">
+          <label>Lugar</label>
+          <input
+            type="text"
+            placeholder="Lugar"
+            name="location"
+            value={location}
+            onChange={onChange}
+          />
+        </div>
+        <div className="form-group">
+          <label>Grado Dificultad</label><small> (Valor numerico del 1 al 5)</small>
+          <input
+            type="text"
+            placeholder="Grado dificultad"
+            name="grading"
+            value={grading}
+            onChange={onChange}
+          />
+        </div>
+        <div className="form-group">
+          <label>Cupo</label>
+          <input
+            type="text"
+            placeholder="Cupo"
+            name="quota"
+            value={quota}
+            onChange={onChange}
+          />
+        </div>
+        <div className="form-group">
+          <label>Reservas</label>
+          <input
+            type="text"
+            placeholder="Reservas"
+            name="reservations"
+            value={reservations}
+            onChange={onChange}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="checkbox"
+            name="published"
+            checked={published}
+            onChange={onChange}
+          />
+          <label>Publicado</label>
+        </div>
+        <input type="submit" className="btn btn-primary" value="Aceptar" />
+        <input type="button" className="btn btn-secondary" value="Cancelar" onClick={() => navigate('/dashboard')} />
+      </form>
+    </section>
+  );
 };
-    
+
 
 EditTrip.propTypes = {
-    updateTrip: PropTypes.func.isRequired
+  updateTrip: PropTypes.func.isRequired
 };
 
 const mapStateToProps = (state) => ({
-    trip: state.trip
-  });
+  trip: state.trip
+});
 
-  
+
 export default connect(mapStateToProps, { updateTrip })(EditTrip);
--- routes/api/trips.js
+++ routes/api/trips.js
@@ -17,39 +17,24 @@ cloudinary.config({
   api_secret: config.get('api_secret'),
 });
 
-router.get('/updateall', 
-    async (req, res) => {
-      console.log('ACAAAAAAAAAAAAAAA');
-      try {
-        Trip.updateMany( {}, { $set: { published: true} } );
-        res.status(200).json({msg: 'OK'});
-      }
-      catch(err) {
-        console.error(err.message);
-
-        res.status(500).send('Server Error');
-      }
-      
-    }
-);
 
 // @route    GET api/trips/:id
 // @desc     Get trip by ID
 // @access   Public
-router.get('/:id', 
-    checkObjectId('id'),
-    async (req, res) => {
+router.get('/:id',
+  checkObjectId('id'),
+  async (req, res) => {
     try {
       const trip = await Trip.findById(req.params.id);
-  
+
       if (!trip) {
         return res.status(404).json({ msg: 'Evento no encontrado' });
       }
-  
+
       res.json(trip);
     } catch (err) {
       console.error(err.message);
-  
+
       res.status(500).send('Server Error');
     }
   });
@@ -57,35 +42,35 @@ router.get('/:id',
 // @route    GET api/trips/?q&df=&dt&limit&page
 // @desc     Get Trips by query
 // @access   Public
-router.get('/', 
-    async (req, res) => {
+router.get('/',
+  async (req, res) => {
     try {
       const currentDate = new Date();
 
-      const query = req.query.q ? req.query.q :  "";
+      const query = req.query.q ? req.query.q : "";
       const dateFrom = req.query.df ? req.query.df : "1900-01-01";
       const dateTo = req.query.dt ? req.query.dt : moment(currentDate).add(5, 'year').format('YYYY-MM-DD');
       const limit = req.query.limit && !isNaN(req.query.limit) ? parseInt(req.query.limit) : 100;
       let page = 1;
       if (req.query.page && !isNaN(req.query.page) && parseInt(req.query.page) > 0)
-          page = parseInt(req.query.page);
+        page = parseInt(req.query.page);
 
       const sort = req.query.sort ? req.query.sort : "date";
       const order = req.query.order ? req.query.order : "-1";
-      const category = req.query.category ? req.query.category :  "";
+      const category = req.query.category ? req.query.category : "";
 
       let db_query = {
-          date:{ $gte: new Date(dateFrom), $lt: new Date(dateTo)},
-          $or: [
-            { title: { $regex: query, '$options' : 'i' }},
-            { subtitle: { $regex: query, '$options' : 'i' }},
-            { description: { $regex: query, '$options' : 'i' }},
-            { location: { $regex: query, '$options' : 'i' }},
-          ]
+        date: { $gte: new Date(dateFrom), $lt: new Date(dateTo) },
+        $or: [
+          { title: { $regex: query, '$options': 'i' } },
+          { subtitle: { $regex: query, '$options': 'i' } },
+          { description: { $regex: query, '$options': 'i' } },
+          { location: { $regex: query, '$options': 'i' } },
+        ]
       };
 
       if (category !== "")
-        db_query = {...db_query, category: category}
+        db_query = { ...db_query, category: category }
 
       const totalItems = await Trip
         .find(db_query)
@@ -94,10 +79,10 @@ router.get('/',
       const trips = await Trip
         .find(db_query)
         .limit(limit)
-        .skip(limit*(page-1))
-        .sort({[sort]: order});
-        
-      res.json({ 
+        .skip(limit * (page - 1))
+        .sort({ [sort]: order });
+
+      res.json({
         "metadata": {
           "query": query,
           "total": totalItems,
@@ -106,16 +91,16 @@ router.get('/',
           "page": page,
           "dateFrom": dateFrom,
           "dateTo": dateTo
-        },          
+        },
         "data": trips
       });
-  
+
       if (!trips) {
         return res.status(404).json({ msg: 'Evento no encontrado' });
       }
     } catch (err) {
       console.error(err.message);
-  
+
       res.status(500).send('Server Error');
     }
   });
@@ -123,84 +108,84 @@ router.get('/',
 // @route   POST api/trips
 // @desc    Add Trip
 // @access  Private
-router.post('/', 
+router.post('/',
+  [
+    auth,
     [
-        auth,
-        [
-            check('title', 'Titulo es requerido').not().isEmpty(),
-            check('subtitle', 'Subtitulo es requerido').not().isEmpty(),
-            check('description', 'Descripcion es requerido').not().isEmpty(),
-            check('location', 'Lugar es requerido').not().isEmpty(),
-        ]
-    ],
-    async (req,res) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-          return res.status(400).json({ errors: errors.array() });
-        }
-
-        const { title, subtitle, category, description, itinerary, date, duration, price, location, grading, quota, reservations, suggested_equipment } = req.body;
-
-        try {
-            let newTrip = new Trip({
-                title,
-                subtitle,
-                category,
-                description,
-                itinerary,
-                date,
-                duration,
-                price,
-                location,
-                grading,
-                quota,
-                reservations,
-                suggested_equipment,
-                user: req.user.id
-            });
-
-            const trip = await newTrip.save();
-            res.json(trip);
-        } catch (err) {
-            console.error(err);
-            res.status(500).send(err);
-        }
-        
+      check('title', 'Titulo es requerido').not().isEmpty(),
+      check('subtitle', 'Subtitulo es requerido').not().isEmpty(),
+      check('description', 'Descripcion es requerido').not().isEmpty(),
+      check('location', 'Lugar es requerido').not().isEmpty(),
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
+
+    const { title, subtitle, category, description, itinerary, date, duration, price, location, grading, quota, reservations, suggested_equipment } = req.body;
+
+    try {
+      let newTrip = new Trip({
+        title,
+        subtitle,
+        category,
+        description,
+        itinerary,
+        date,
+        duration,
+        price,
+        location,
+        grading,
+        quota,
+        reservations,
+        suggested_equipment,
+        user: req.user.id
+      });
+
+      const trip = await newTrip.save();
+      res.json(trip);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send(err);
+    }
+
+  }
 );
 
 // @route    DELETE api/trips/:id
 // @desc     Delete a Trip
 // @access   Private
-router.delete('/:id', 
-  auth,  
+router.delete('/:id',
+  auth,
   checkObjectId('id'),
   async (req, res) => {
     try {
       const trip = await Trip.findById(req.params.id);
-  
+
       if (!trip) {
         return res.status(404).json({ msg: 'Evento no encontrado' });
       }
-      
+
       if (trip.images.length > 0) {
-        const images = trip.images.map(function(item) {
+        const images = trip.images.map(function (item) {
           return item["public_id"];
         })
-  
-        cloudinary.api.delete_resources(images, function(err, _result){
+
+        cloudinary.api.delete_resources(images, function (err, _result) {
           if (err) {
             res.status(500).send(res);
-          }           
+          }
         })
       }
 
       await trip.remove();
-  
+
       res.json({ msg: 'Evento eliminado' });
     } catch (err) {
       console.error(err.message);
-  
+
       return res.status(500).json({ msg: 'Server error' });
     }
   });
@@ -208,31 +193,31 @@ router.delete('/:id',
 // @route   PUT api/trips/:id
 // @desc    Update Trip
 // @access  Private
-router.put('/:id', 
-    checkObjectId('id'),
+router.put('/:id',
+  checkObjectId('id'),
+  [
+    auth,
     [
-        auth,
-        [
-            check('title', 'Titulo es requerido').not().isEmpty(),
-            check('subtitle', 'Subtitulo es requerido').not().isEmpty(),
-            check('description', 'Descripcion es requerido').not().isEmpty(),
-            check('location', 'Lugar es requerido').not().isEmpty(),
-        ]
-    ],
-    async (req,res) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-          return res.status(400).json({ errors: errors.array() });
-        }
-        try {
-            const trip = await Trip.findByIdAndUpdate(req.params.id, req.body, {new: true});
-            res.json(trip);
-        } catch (err) {
-            console.error(err);
-            res.status(500).send(err);
-        }
-        
+      check('title', 'Titulo es requerido').not().isEmpty(),
+      check('subtitle', 'Subtitulo es requerido').not().isEmpty(),
+      check('description', 'Descripcion es requerido').not().isEmpty(),
+      check('location', 'Lugar es requerido').not().isEmpty(),
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
+    try {
+      const trip = await Trip.findByIdAndUpdate(req.params.id, req.body, { new: true });
+      res.json(trip);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send(err);
+    }
+
+  }
 );
 
 
