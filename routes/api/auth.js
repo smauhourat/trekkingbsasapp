0d365220dc27ab3969f044156d3dbfb84ed5a637
SMauhourat <santiagomauhourat@hotmail.com>
2023-04-19 07:27:03 -0300

    forgot password api

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const nodemailer = require('nodemailer');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const User = require('../../models/User');
@@ -6,6 +7,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
 const { check, validationResult } = require('express-validator');
+const JWT_SECRET = config.get('jwtSecret');
 
 // @route   GET api/auth
 // @desc    Test route
@@ -59,7 +61,7 @@ router.post(
 
             jwt.sign(
                 payload, 
-                config.get('jwtSecret'),
+                JWT_SECRET,
                 { expiresIn: 360000 },
                 (err, token) => {
                     if (err) throw err;
@@ -73,5 +75,119 @@ router.post(
         }
     }
 );
+
+// @route   POST api/auth/forgot-password
+// @desc    Forgot Password, send restore link by mail
+// @access  Public
+router.post("/forgot-password", async (req, res) => {
+  const { email } = req.body;
+  try {
+    const oldUser = await User.findOne({ email });
+    if (!oldUser) {
+      return res.json({ status: "Usuario no existe!!" });
+    }
+    const secret = JWT_SECRET + oldUser.password;
+    const token = jwt.sign({ email: oldUser.email, id: oldUser._id }, secret, {
+      expiresIn: "5m",
+    });
+    const link = `api/auth/reset-password/${oldUser._id}/${token}`;
+
+    const transporter = nodemailer.createTransport({
+        host: config.get('contact_host'), //replace with your email provider
+        port: config.get('contact_port'), //replace with your
+        auth: {
+          user: config.get('contact_user'), //process.env.EMAIL,
+          pass: config.get("contact_pwd") //process.env.PASSWORD
+        }
+      });
+    
+    // verify connection configuration
+    transporter.verify(function(error, success) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log("Server is ready to take our email messages");
+        }
+      });
+
+    var mail = {
+        from: config.get('contact_user'),
+        to: email,
+        subject: 'Reestablecer Contraseña',
+        text: link
+      }        
+    
+    transporter.sendMail(mail, (err, data) => {
+        if (err) {
+          res.json({
+            status: 'fail'
+          })
+        } else {
+          res.json({
+           status: 'success'
+          })
+        }
+      });
+
+
+    console.log(link);
+    res.json(link);
+  } catch (error) { }
+});
+
+// @route   GET api/auth/reset-password/:id/:token
+// @desc    Forgot Password, send restore link by mail
+// @access  Public
+router.get("/reset-password/:id/:token", async (req, res) => {
+    const { id, token } = req.params;
+    console.log(req.params);
+    const oldUser = await User.findOne({ _id: id });
+    if (!oldUser) {
+      return res.json({ status: "Usuario no existe!!" });
+    }
+    //console.log(oldUser);
+    const secret = JWT_SECRET + oldUser.password;
+    try {
+      const verify = jwt.verify(token, secret);
+      res.json({ email: verify.email, status: "No verificado" });
+    } catch (error) {
+      console.log(error);
+      res.send("No verificado");
+    }
+  });
+
+// @route   POST api/auth/reset-password/:id/:token
+// @desc    Reset Password, send new password
+// @access  Public
+router.post("/reset-password/:id/:token", async (req, res) => {
+    const { id, token } = req.params;
+    const { password } = req.body;
+  
+    const oldUser = await User.findOne({ _id: id });
+    if (!oldUser) {
+      return res.json({ status: "Usuario no existe!!" });
+    }
+    const secret = JWT_SECRET + oldUser.password;
+    try {
+      const verify = jwt.verify(token, secret);
+      var salt = await bcrypt.genSalt(10);
+      const encryptedPassword = await bcrypt.hash(password, salt);
+      await User.updateOne(
+        {
+          _id: id,
+        },
+        {
+          $set: {
+            password: encryptedPassword,
+          },
+        }
+      );
+  
+      res.json({ email: verify.email, status: "verificado" });
+    } catch (error) {
+      console.log(error);
+      res.json({ status: "Ocurrio un error inesperado", error: error });
+    }
+  });
 
 module.exports = router;
--- routes/api/contact.js
+++ routes/api/contact.js
@@ -6,23 +6,7 @@ const { getMaxListeners } = require('../../models/User');
 
 const config = require('config');
 
-const transporter = nodemailer.createTransport({
-    host: config.get('contact_host'), //replace with your email provider
-    port: config.get('contact_port'), //replace with your
-    auth: {
-      user: config.get('contact_user'), //process.env.EMAIL,
-      pass: config.get("contact_pwd") //process.env.PASSWORD
-    }
-  });
 
-// verify connection configuration
-transporter.verify(function(error, success) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("Server is ready to take our messages");
-    }
-  });  
 
 // @route    GET api/contact
 // @desc     Send Contact Info
@@ -41,6 +25,24 @@ router.post('/',
         }
 
         const { title, email, subject, message } = req.body;
+
+        const transporter = nodemailer.createTransport({
+          host: config.get('contact_host'), //replace with your email provider
+          port: config.get('contact_port'), //replace with your
+          auth: {
+            user: config.get('contact_user'), //process.env.EMAIL,
+            pass: config.get("contact_pwd") //process.env.PASSWORD
+          }
+        });
+      
+      // verify connection configuration
+      transporter.verify(function(error, success) {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log("Server is ready to take our contact messages");
+          }
+        });  
 
         var mail = {
             from: email,
