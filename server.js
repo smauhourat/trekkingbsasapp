6994a1ced0deacbcbe0730913c31fdbaff875966
SMauhourat <santiagomauhourat@hotmail.com>
2023-05-01 19:26:56 -0300

    deploy to vercel

--- server.js
+++ server.js
@@ -33,13 +33,21 @@ app.use('/api/trips/:id/images', require('./routes/api/images'));
 app.use('/api/contact', require('./routes/api/contact'));
 
 // Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
+//if (process.env.NODE_ENV === 'production') {
     // Set static folder
     app.use(express.static('client/build'));
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(
+            path.join(__dirname, "./client/build/index.html"),
+            function (err) {
+                if (err) {
+                  res.status(500).send(err);
+                }
+              }
+        );
+        //res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     })
-}
+//}
 
 const PORT = process.env.PORT || 5000;
 
