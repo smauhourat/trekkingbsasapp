fc5eb94a56955e029e4e0022167aabf6c960d07b
SMauhourat <santiagomauhourat@hotmail.com>
2023-05-07 22:44:41 -0300

    fix log db connect

--- config/db.js
+++ config/db.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 db = global.env.mongoUri;
-const db_name = console.log(db.substring(db.indexOf('/', 14)+1, db.indexOf('?')));
+const db_name = db.substring(db.indexOf('/', 14)+1, db.indexOf('?'));
 
 const connectDB = async () => {
     try {
         await mongoose.connect(db);
 
-        console.log(`MongoDb Connected...`)
+        console.log(`MongoDb Connected...${db_name}`)
     } catch (err) {
         console.error(err);
         // Exit process with failure
