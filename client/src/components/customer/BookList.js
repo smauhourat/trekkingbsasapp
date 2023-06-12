fae4871a79cca2d4361c622dbe6dbdad06a950c2
SMauhourat <santiagomauhourat@hotmail.com>
2023-06-12 09:23:23 -0300

    books list

---
+++ client/src/components/customer/BookList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types'
+import { getBooks } from '../../actions/book';
+
+
+const BookList = ({ getBooks, book: { books, loading } }) => {
+
+}
