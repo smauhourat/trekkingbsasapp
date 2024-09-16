0777c5586111d3675d6aab3e7330a248ebae8e53
SMauhourat <santiagomauhourat@hotmail.com>
2024-09-15 22:15:29 -0300

    send mail test

--- routes/api/tests.js
+++ routes/api/tests.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { sendEmail } = require('../../utils/emailService')
 
 router.get('/', async (req, res) => {
   const hostname = req.headers.host; // Get the hostname
@@ -15,6 +16,30 @@ router.get('/', async (req, res) => {
     protocol: protocol,
     host: host
   })
+
+  const mail = {
+    from: global.env.contact_user,
+    to: 'santiagomauhourat@hotmail.com', //user.email,
+    subject: `Reserva AAAA`,
+    text: "boolDetailslink",
+    html: `<p>Hola Santiago gracias por elegirnos!!</p><br><p>Recibimos tu RESERVA correctamente, COD: AAAA</p><p><a href="http://www.google.com">Ver Detalle</a></p>`
+  }
+
+  console.log(mail)
+
+  // try {
+  //   // await sendEmail(mail)
+  //   return ({
+  //     status: 'success',
+  //     message: 'El mail ha sido enviado con exito'
+  //   })
+  // } catch (err) {
+  //   return ({
+  //     status: 'fail',
+  //     message: 'Error enviando el mail'
+  //   })
+  // }
+
 }) 
 
 module.exports = router
