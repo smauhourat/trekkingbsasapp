b324c88ba9cd3aced45346a7dbe7d2236f3a269f
SMauhourat <santiagomauhourat@hotmail.com>
2024-09-23 08:30:48 -0300

    fix verify email 3

--- client/src/components/customer/VerifyEmail.js
+++ client/src/components/customer/VerifyEmail.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from 'react'
-import { Link, useParams, useNavigate } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import api from '../../utils/api'
 import Spinner from '../layout/Spinner';
 
 const VerifyEmail = () => {
-  const navigate = useNavigate()
   const { id, token } = useParams()
   const [isMailVerified, setIsMailVerified] = useState(false)
   const [messageUser, setMessageUser] = useState('Espere por favor, estamos validando su direccion de email.')
@@ -17,7 +16,7 @@ const VerifyEmail = () => {
         setMessageUser(res.data.message)
       }
     }
-    console.log('useEffect()')
+    
     validateEmail()
   }, [id, token, isMailVerified])
 
