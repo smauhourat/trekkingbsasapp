1c19dc08811ef7e9cd90c3e52d0888f9a253aaff
Santiago Mauhourat <smauhourat@aadim.org.ar>
2024-09-26 16:40:10 -0300

    add createdAt in customers list

--- client/src/components/dashboard/CustomersList.js
+++ client/src/components/dashboard/CustomersList.js
@@ -5,7 +5,7 @@ import Spinner from '../layout/Spinner';
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import { setAlert } from '../../actions/alert'
-import { formatDateTimeBsAs } from '../../utils/dateHelper'
+import { formatDateTimeBsAs, formatDate } from '../../utils/dateHelper'
 import environment from '../../utils/environment'
 import Pagination from '../layout/Pagination'
 
@@ -60,29 +60,32 @@ const CustomersList = ({ setAlert }) => {
                     <thead>
                         <tr>
                             <th width='35%'><div className='link' onClick={() => handleOnChangeOrder('last_name')}>Nombre {sortOrderIcon('last_name')}</div></th>
-                            <th width='35%'><div className='link'>Email</div></th>
+                            <th width='30%'><div className='link'>Email</div></th>
                             <th width='15%'><div className='link' onClick={() => handleOnChangeOrder('dni')}>DNI {sortOrderIcon('dni')}</div></th>
                             <th width='10%'><div className='link'>Telefono</div></th>
                             <th width='5%'><div className='link'>Ult. Acceso</div></th>
+                            <th width='5%'><div className='link'>F. Alta</div></th>
                         </tr>
                     </thead>
                     <tbody>
-                        {isPending && (<tr><td colSpan='5'><Spinner /></td></tr>)}
+                        {isPending && (<tr><td colSpan='6'><Spinner /></td></tr>)}
                         {!isPending && (
                             data?.customers?.map((customer, rowIndex) =>
-                                (<tr key={rowIndex}>
+                                (
+                                <tr key={rowIndex}>
                                     <td>{customer.last_name}, {customer.first_name}</td>
                                     <td>{customer?.user?.email}</td>
                                     <td>{customer.dni}</td>
                                     <td>{customer.phone}</td>
                                     <td className='no-wrap'>{customer?.user?.last_access ? formatDateTimeBsAs(customer?.user?.last_access) : ""}</td>
-                                    </tr>)
+                                    <td className='no-wrap'>{customer?.createdAt ? formatDate(customer?.createdAt) : ""}</td>
+                                </tr>)
                                 )
                             )}
                     </tbody>
                     <tfoot>
                         <tr>
-                            <td colSpan='5'>
+                            <td colSpan='6'>
                                 <Pagination 
                                     currentPage={currentPage}
                                     totalItems={data?.metadata.total}
