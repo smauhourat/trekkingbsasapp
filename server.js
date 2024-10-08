69fd8890af3222a0464992dd6fbe2472915733aa
Santiago Mauhourat <smauhourat@aadim.org.ar>
2024-10-08 13:17:43 -0300

    change log to test

--- server.js
+++ server.js
@@ -73,4 +73,4 @@ const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
 
-logger.info(`Server started on port ${PORT}`)
+logger.info(`Server started on port: ${PORT}`)
